90adc2d240743817c4117c85785fe32de5235cf7
Dragan Pepic <d.pepic@hotmail.com>
2020-06-25 17:37:31 +0200

    Add project files.

---
+++ ValidacijaII/Server/Hab.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+using ValidacijaII.Shared;
+
+namespace ValidacijaII.Server
+{
+	public class Hab : Hub
+	{
+		public void ZaOsobu(ValidacijaII.Shared.Osoba o)
+		{
+			Console.WriteLine($"{o.Ime} -- {o.Prezime}");
+			var os = new ValidacijaII.Shared.Osoba();
+			os.Ime = "Neko";
+			os.Prezime = "Nekic";
+			Clients.Caller.SendAsync("Test", os);
+		}
+	}
+}
---
+++ ValidacijaII/Shared/Osoba.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FluentValidation;
+
+namespace ValidacijaII.Shared
+{
+	public class ValidatorZaOsobu : AbstractValidator<Osoba>
+	{
+		public ValidatorZaOsobu()
+		{
+			RuleFor(o => o.Ime).NotEmpty().WithMessage("Ne sme prazno!")
+								.MinimumLength(3).WithMessage("Prekratko!")
+								.MaximumLength(10).WithMessage("Predugo!");
+			RuleFor(o => o.Prezime).NotEmpty().WithMessage("Nista prazno!");
+		}
+	}
+
+	public class Osoba
+	{
+		public string Ime { get; set; }
+		public string Prezime { get; set; }
+	}
+}
